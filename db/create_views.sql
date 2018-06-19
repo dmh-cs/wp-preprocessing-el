0326d52b16f3fa178c68444747095239bf9322fe
Dany Haddad <dhaddad@cognitivescale.com>
2018-06-19 15:47:27 -0500

    add a view for entities and mentions

--- db/create_views.sql
+++ db/create_views.sql
@@ -8,10 +8,19 @@ CREATE OR REPLACE VIEW mention_by_entity AS
 
 CREATE OR REPLACE VIEW category_by_page AS SELECT p.id AS page_id, c.category AS category
        FROM categories c INNER JOIN page_categories pc ON c.id = pc.category_id
-                       INNER JOIN pages p ON p.id = pc.page_id;
+                         INNER JOIN pages p ON p.id = pc.page_id;
 
 CREATE OR REPLACE VIEW entity_by_page AS
        SELECT e.id AS entity_id,
               p.id AS page_id,
               e.text AS entity
        FROM entities e INNER JOIN pages p ON p.title = e.text;
+
+CREATE OR REPLACE VIEW entity_mentions_text AS
+       SELECT m.text AS mention,
+              m.id AS mention_id,
+              m.page_id AS page_id,
+              e.text AS entity,
+              e.id AS entity_id
+              FROM mentions m INNER JOIN entity_mentions em ON em.mention_id = m.id
+                              INNER JOIN entities e on em.entity_id = e.id;
