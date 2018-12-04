63e5394d82a1e40fa8fadd1ece44a588dfd4f49c
Dany Haddad <dhaddad@cognitivescale.com>
2018-12-03 21:27:59 -0600

    just a file name

--- src/scripts/merge.py
+++ src/scripts/merge.py
@@ -5,12 +5,13 @@ directory = os.fsencode('out')
 ctr = 0
 current_fh = open('out0', 'w+')
 num_files = 0
-for fh in os.listdir(directory):
+for f in os.listdir(directory):
   if ctr == 10000:
     current_fh.close()
     current_fh = open(f'out{num_files}', 'w+')
     num_files += 1
   current_fh.write('\n----------.----------\n')
-  current_fh.write(fh.read())
+  with open(f, 'r') as fh:
+    current_fh.write(fh.read())
   ctr += 1
 current_fh.close()
