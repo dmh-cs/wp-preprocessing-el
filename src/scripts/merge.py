c0fd658d45f53ac13b859f626b162eb7de5df2ff
Dany Haddad <dhaddad@cognitivescale.com>
2018-12-03 21:34:20 -0600

    reset to 0

--- src/scripts/merge.py
+++ src/scripts/merge.py
@@ -8,8 +8,9 @@ num_files = 0
 for f in os.listdir(directory):
   if ctr == 10000:
     current_fh.close()
-    current_fh = open(f'out{num_files}', 'a+')
+    current_fh = open(f'combined/out{num_files}', 'a+')
     num_files += 1
+    ctr = 0
   current_fh.write('\n----------.----------\n')
   with open('out/'+f.decode("utf-8"), 'r') as fh:
     current_fh.write(fh.read())
