1ee24da17eac6a54476fa9b861897b1de7764cec
Dany Haddad <dhaddad@cognitivescale.com>
2018-12-03 21:29:15 -0600

    cast to string

--- src/scripts/merge.py
+++ src/scripts/merge.py
@@ -3,15 +3,15 @@ import os
 directory = os.fsencode('out')
 
 ctr = 0
-current_fh = open('out0', 'w+')
+current_fh = open('out0', 'a+')
 num_files = 0
 for f in os.listdir(directory):
   if ctr == 10000:
     current_fh.close()
-    current_fh = open(f'out{num_files}', 'w+')
+    current_fh = open(f'out{num_files}', 'a+')
     num_files += 1
   current_fh.write('\n----------.----------\n')
-  with open(f, 'r') as fh:
+  with open(str(f), 'r') as fh:
     current_fh.write(fh.read())
   ctr += 1
 current_fh.close()
