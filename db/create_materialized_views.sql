b633fe6b7aa19d1192f6c873abb1edac816fa0a2
Dany Haddad <dhaddad@cognitivescale.com>
2019-06-14 10:56:23 -0500

    support materialized views

---
+++ db/create_materialized_views.sql
@@ -0,0 +1,8 @@
+CREATE TABLE entity_mentions_text_tbl AS
+       SELECT mention,
+              offset,
+              mention_id,
+              page_id,
+              entity,
+              entity_id
+       FROM entity_mentions_text;
---
+++ db/drop_materialized_views.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS entity_mentions_text_tbl;
